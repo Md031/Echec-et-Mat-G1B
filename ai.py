ae5e783f0c875aaba911213d1f22f911d4b1844f
RahmanTektas <rahman.tektas@ulb.be>
2023-11-09 14:35:48 +0100

    iplementing evaluation method

--- ai.py
+++ ai.py
@@ -1,22 +1,34 @@
 import random 
 import chess 
-import pieceValues
+from pieceValues import *
 
 class Ai:   #Interface class
+    def __init__(self):
+        self.color = None
+
     def move(board):
         raise Exception("NotImplementedException")
+    
+    def set_color(self, color):
+        self.color = color
 
 class Random(Ai):
+    def __init__(self):
+        super().__init__()
+
     def move(self, board):
         move = random.choice(list(board.legal_moves))
         return move.uci()
     
 class Minimax(Ai):
+    def __init__(self):
+        super().__init__()
+
     # not complete
     def move(self, board, depth):
         bestMove = float('-inf')
         for move in board.legal_moves:
-            move = chess.push_uci(move)
+            board.push_uci(move.uci())
 
             value = max(bestMove, self.max_value( board, depth -1))
             board.pop()     #undo the last move
@@ -24,16 +36,16 @@ class Minimax(Ai):
             if(value > bestMove):
                 bestMove = value
                 bestMoveFinal = move
-        return bestMoveFinal
+                
+        return bestMoveFinal.uci()
     
     def max_value(self, board, depth):
         if depth <= 0 or board.is_game_over :
             return self.evaluation(board)
 
         best_move = float('-inf')
-        for legal_move in board.legal_moves:
-            move = chess.Move.from_uci(str(legal_move))
-            board.push(move)
+        for move in board.legal_moves:
+            board.push_uci(move.uci)()
 
             best_move = max(best_move,  self.min_value(board, depth - 1))
             board.pop()
@@ -44,22 +56,47 @@ class Minimax(Ai):
         if depth <= 0 or board.is_game_over :
             return self.evaluation(board)
 
-        best_move = None
         best_move = float('inf')
-        for legal_move in board.legal_moves:
-            move = chess.Move.from_uci(str(legal_move))
-            board.push(move)
+        for move in board.legal_moves:
+            board.push_uci(move.uci())
 
             best_move = max(best_move, self.max_value(board, depth - 1))
             board.pop()
 
         return best_move
                 
-    def evaluation(self):
-        #code a method that evaluates the board state using pieceValues tables. 
+    def evaluation(self, board):
+        total_evaluation = 0
+        
+        for square in chess.SQUARES:
+            piece = board.piece_at(square)
+            if piece is not None:
 
-        # to complete
-        pass
+                if piece.color == chess.WHITE:  #Counts value of the piece using the white player table
+                    if piece.piece_type == chess.KING:  #if the current piece is a king we need to check accordingly if its the end game or not
+                            if self.is_end_game(board) :
+                                total_evaluation += PIECE_VALUES[piece.piece_type] + PIECE_TABLES_WHITE[KING_END_GAME][square]
+                            else :
+                                total_evaluation += PIECE_VALUES[piece.piece_type] + PIECE_TABLES_WHITE[KING_MIDDLE_GAME][square]
+                                
+                    else:   #its not a king
+                            total_evaluation += PIECE_TABLES_WHITE[piece.piece_type - 1][square] # PIECE_TABLES[piece.piece_type - 1] because piece.piece_type starts at index 1 while our list starts at index 0 (to match the type in the table basically)
+
+                else:    #Counts value of the piece for the black player using the other table
+                    if piece.piece_type == chess.KING:
+                            if self.is_end_game(board) :
+                                total_evaluation -= PIECE_VALUES[piece.piece_type] + PIECE_TABLES_BLACK[KING_END_GAME][square]
+                            else :
+                                total_evaluation -= PIECE_VALUES[piece.piece_type] + PIECE_TABLES_BLACK[KING_MIDDLE_GAME][square]
+                                
+                    else:
+                            total_evaluation -= PIECE_VALUES[piece.piece_type] * PIECE_TABLES_BLACK[piece.piece_type - 1][square] # PIECE_TABLES[piece.piece_type - 1] because piece.piece_type starts at index 1 while our list starts at index 0 (to match the type in the table basically)
+
+        
+        if(self.color == chess.BLACK):  #if we evaluate for the black player we need to invert this value this is easier than creating more conditions in the loop up there
+            total_evaluation = -total_evaluation
+
+        return total_evaluation
 
     def is_end_game(self, board):
         # returns true if the current state of the game is ¨the end game
@@ -81,17 +118,15 @@ class Minimax(Ai):
         for square in chess.SQUARES:
             piece = board.piece_at(square)
             if piece is not None:
-                if piece.piece_type != chess.QUEEN: 
-                    side_piece_count[piece.color] += pieceValues.PIECE_VALUES[piece.piece_type]     # this adds the value of the piece to the counter
+                if piece.piece_type != chess.QUEEN and piece.piece_type != chess.KING: 
+                    side_piece_count[piece.color] += PIECE_VALUES[piece.piece_type]     # this adds the value of the piece to the counter
 
         # Check the conditions for each side
         for side in [chess.WHITE, chess.BLACK]:
             if any(piece.piece_type == chess.QUEEN and piece.color == side for piece in board.piece_map().values()):    #checks if there is a queen of the given color
                 if side_piece_count[side] > 300:    #checks if there is more than one minor piece or three pawns    
-                    has_more_pieces = True #side has more than one minor piece or other pieces along with a queen.")
-                else:
+                    return True     #if one side that has a queen has more than one minor piece we stop and return true.
+                else:   #else we check for the other side
                     has_more_pieces = False #side has at most one minor piece along with a queen.")
 
-        if not has_more_pieces:
-            return True
-        return False
+        return has_more_pieces
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class Game:
         self.player_1 = player_1
         self.player_2 = player_2
 
+        self.player_1.set_color(chess.WHITE)
+        self.player_2.set_color(chess.BLACK)
 
     def who(self, player):
         return "White" if player == chess.WHITE else "Black"
@@ -17,7 +19,7 @@ class Game:
         try:
             while not board.is_game_over(claim_draw=True):
                 if board.turn == chess.WHITE:
-                    uci = self.player_1.move(board)
+                    uci = self.player_1.move(board, 20)
                 else:
                     uci = self.player_2.move(board)
                 board.push_uci(uci)
@@ -43,6 +45,6 @@ class Game:
 
 board = chess.Board()
 
-game = Game(board, player_1 = Random(), player_2 = Human())
+game = Game(board, player_1 = Minimax(), player_2 = Random())
 
 game.play()
