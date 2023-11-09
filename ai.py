21eea946491ec6eed09d323ad81f403d0660ec20
RahmanTektas <rahman.tektas@ulb.be>
2023-11-09 04:48:34 +0100

    Adding pieceValues for evaluation

--- ai.py
+++ ai.py
@@ -1,17 +1,16 @@
 import random 
 import chess 
+import pieceValues
 
 class Ai:   #Interface class
     def move(board):
         raise Exception("NotImplementedException")
-
 
 class Random(Ai):
     def move(self, board):
         move = random.choice(list(board.legal_moves))
         return move.uci()
     
-
 class Minimax(Ai):
     # not complete
     def move(self, board, depth):
@@ -26,9 +25,6 @@ class Minimax(Ai):
                 bestMove = value
                 bestMoveFinal = move
         return bestMoveFinal
-
-
-
     
     def max_value(self, board, depth):
         if depth <= 0 or board.is_game_over :
@@ -41,9 +37,9 @@ class Minimax(Ai):
 
             best_move = max(best_move,  self.min_value(board, depth - 1))
             board.pop()
+
         return best_move
                 
-
     def min_value(self, board, depth):
         if depth <= 0 or board.is_game_over :
             return self.evaluation(board)
@@ -60,12 +56,42 @@ class Minimax(Ai):
         return best_move
                 
     def evaluation(self):
-        #code a method that evaluates the board state, an idea is to count the number of pieces * their values
+        #code a method that evaluates the board state using pieceValues tables. 
 
-        # pion : 1 point
-        # cavalier : 3 points
-        # fou : 3 points
-        # tour : 5 points
-        # dame : 9 points
-        # roi : je pense float(+inf)
+        # to complete
         pass
+
+    def is_end_game(self, board):
+        # returns true if the current state of the game is ¨the end game
+        # measured by two metrics:
+        # 1. there is no queens on the board
+        # 2. Every side which has a queen has additionally no other pieces or one minorpiece maximum. A minor piece is either a bishop, knight or three pawns since it has the same value.
+
+        # This function is important for the evaluation of the king's position since the evaluation is different in the endgame
+
+        queens_present = any(piece.piece_type == chess.QUEEN for piece in board.piece_map().values()) #checks if theres is queens on the board
+
+        if not queens_present:
+            return True
+
+        # Dictionary to store the count of pieces for each side
+        side_piece_count = {chess.WHITE: 0, chess.BLACK: 0}
+
+        # Iterate through all the pieces on the board
+        for square in chess.SQUARES:
+            piece = board.piece_at(square)
+            if piece is not None:
+                if piece.piece_type != chess.QUEEN: 
+                    side_piece_count[piece.color] += pieceValues.PIECE_VALUES[piece.piece_type]     # this adds the value of the piece to the counter
+
+        # Check the conditions for each side
+        for side in [chess.WHITE, chess.BLACK]:
+            if any(piece.piece_type == chess.QUEEN and piece.color == side for piece in board.piece_map().values()):    #checks if there is a queen of the given color
+                if side_piece_count[side] > 300:    #checks if there is more than one minor piece or three pawns    
+                    has_more_pieces = True #side has more than one minor piece or other pieces along with a queen.")
+                else:
+                    has_more_pieces = False #side has at most one minor piece along with a queen.")
+
+        if not has_more_pieces:
+            return True
+        return False
---
+++ pieceValues.py
@@ -0,0 +1,78 @@
+# This section is inspired by the work of famous polish chess player Tomasz Michniewski 
+# The idea is to create tables that represent the value of a type of piece in the board given its position
+
+PIECE_VALUES = [None, 100, 300, 300, 500, 900, 0]   #maps out the values of the pieces. 
+                                                    # by default in the chess library these are the values chess.PAWN = 1, chess.KNIGHT= 2, chess.BISHOP= 3, chess.ROOK= 4, chess.QUEEN= 5, chess.KING= 6
+                                                    #this is why the index 0 is None
+                                                    #We can get then simply get the value of a piece with PIECE_VALUES[piece.piece_type] 
+
+
+# a pawn is basically a lot stronger in the middle since chess is about controlling the center and the board, and the closer it gets to promotion the higher the value
+pawn = [0,  0,  0,  0,  0,  0,  0,  0,     
+        50, 50, 50, 50, 50, 50, 50, 50,
+        10, 10, 20, 30, 30, 20, 10, 10,
+        5,  5, 10, 25, 25, 10,  5,  5,
+        0,  0,  0, 20, 20,  0,  0,  0,
+        5, -5,-10,  0,  0,-10, -5,  5,
+        5, 10, 10,-20,-20, 10, 10,  5,
+        0,  0,  0,  0,  0,  0,  0,  0]
+
+knight = [-50,-40,-30,-30,-30,-30,-40,-50,
+        -40,-20,  0,  0,  0,  0,-20,-40,
+        -30,  0, 10, 15, 15, 10,  0,-30,
+        -30,  5, 15, 20, 20, 15,  5,-30,
+        -30,  0, 15, 20, 20, 15,  0,-30,
+        -30,  5, 10, 15, 15, 10,  5,-30,
+        -40,-20,  0,  5,  5,  0,-20,-40,
+        -50,-40,-30,-30,-30,-30,-40,-50]
+
+bishop = [-20,-10,-10,-10,-10,-10,-10,-20,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -10,  0,  5, 10, 10,  5,  0,-10,
+        -10,  5,  5, 10, 10,  5,  5,-10,
+        -10,  0, 10, 10, 10, 10,  0,-10,
+        -10, 10, 10, 10, 10, 10, 10,-10,
+        -10,  5,  0,  0,  0,  0,  5,-10,
+        -20,-10,-10,-10,-10,-10,-10,-20]
+
+
+rook = [0,  0,  0,  0,  0,  0,  0,  0,
+        5, 10, 10, 10, 10, 10, 10,  5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        0,  0,  0,  5,  5,  0,  0,  0]
+
+
+queen = [-20,-10,-10, -5, -5,-10,-10,-20,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -10,  0,  5,  5,  5,  5,  0,-10,
+        -5,  0,  5,  5,  5,  5,  0, -5,
+        0,  0,  5,  5,  5,  5,  0, -5,
+        -10,  5,  5,  5,  5,  5,  0,-10,
+        -10,  0,  5,  0,  0,  0,  0,-10,
+        -20,-10,-10, -5, -5,-10,-10,-20]
+
+# A king is safer when in castled in the middle game but we want to activate it in the end game since there is no more piece that can threaten it so we create
+# two different tables. In the end game the kin is stronger when controlling the center.
+king_middle_game = [-30,-40,-40,-50,-50,-40,-40,-30,
+                    -30,-40,-40,-50,-50,-40,-40,-30,
+                    -30,-40,-40,-50,-50,-40,-40,-30,
+                    -30,-40,-40,-50,-50,-40,-40,-30,
+                    -20,-30,-30,-40,-40,-30,-30,-20,
+                    -10,-20,-20,-20,-20,-20,-20,-10,
+                    20, 20,  0,  0,  0,  0, 20, 20,
+                    20, 30, 10,  0,  0, 10, 30, 20]
+
+king_end_game = [-50,-40,-30,-20,-20,-30,-40,-50,
+                -30,-20,-10,  0,  0,-10,-20,-30,
+                -30,-10, 20, 30, 30, 20,-10,-30,
+                -30,-10, 30, 40, 40, 30,-10,-30,
+                -30,-10, 30, 40, 40, 30,-10,-30,
+                -30,-10, 20, 30, 30, 20,-10,-30,
+                -30,-30,  0,  0,  0,  0,-30,-30,
+                -50,-30,-30,-30,-30,-30,-30,-50]
+
+# These values are for white, we can get the same values for black by simply mirroring these 
