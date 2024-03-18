335d1b7d2716bd96b0052bb8d802b157ef47f543
ebeb0001 <ethan.bebelambou.nkoli@ulb.be>
2024-03-18 23:31:43 +0100

    useless comments

--- utils.py
+++ utils.py
@@ -16,31 +16,9 @@ def create_ai(ai_name : str, color : bool) -> None :
 def handle_command_line_arguments():
     playerWhite = None
     playerBlack = None
-    print(len(argv))
     if len(argv) == 3 : # 2 AI's against each other
         playerWhite = create_ai(argv[1], ch.WHITE)
         playerBlack = create_ai(argv[2], ch.BLACK)
     elif len(argv) == 2 : # Human VS Ai
         playerBlack = create_ai(argv[1], ch.BLACK)
-
-    # if len(argv) > 2: # 2 AI's against each other
-    #     if(argv[1].lower() == "random"):
-    #         playerWhite = ai.Random(ch.WHITE)
-    #     elif(argv[1].lower() == "minimax"):
-    #         playerWhite = ai.Minimax(ch.WHITE)
-    #     elif(argv[1].lower() == "neural"):
-    #         playerWhite = ai.NeuronalNetwork(ch.WHITE)
-    #     if(argv[2].lower() == "random"):
-    #         playerBlack = ai.Random(ch.BLACK)
-    #     elif(argv[2].lower() == "minimax"):
-    #         playerBlack = ai.Minimax(ch.BLACK)
-    #     elif(argv[2].lower() == "neural"):
-    #         playerBlack = ai.NeuronalNetwork(ch.BLACK)
-    # elif len(argv) > 1: # Human VS Ai
-    #     if(argv[1].lower() == "random"):
-    #         playerBlack = ai.Random(ch.BLACK)
-    #     elif(argv[1].lower() == "minimax"):
-    #         playerBlack = ai.Minimax(ch.BLACK)
-    #     elif(argv[1].lower() == "neural"):
-    #         playerBlack = ai.NeuronalNetwork(ch.BLACK)
     return playerWhite, playerBlack
